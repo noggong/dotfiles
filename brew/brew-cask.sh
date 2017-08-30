1fa07dc71f189c2b81b057003191030f1307a1ba
MH Jeon <mhjeon@modernlab.io>
2017-08-30 17:42:32 +0900

    Add adobe-acrobat-reader to brew-cask

--- brew/brew-cask.sh
+++ brew/brew-cask.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
+# Cloud
 brew cask install google-chrome
 brew cask install google-drive
-
 brew cask install dropbox
 
-brew cask install 1password
-
+# Utils
 brew cask install slack
-
 brew cask install atext
-
 brew cask install alfred
+brew cask install cleanmymac
+brew cask install spectacle
+brew cask install moom
 
+# Development
 brew cask install iterm2
 brew cask install sublime-text
-
-brew cask install spectacle
-
-brew cask install bettertouchtool
-brew cask install macdown
-brew cask install cleanmymac
 brew cask install dash
-brew cask install moom
+
+# Productivity
+brew cask install 1password
+brew cask install bettertouchtool
+
+# Documentation
+brew cask install macdown
+brew cask install adobe-acrobat-reader
