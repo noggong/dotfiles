67df486405eccb4c2d8fed58afa2b80243cbfdfd
MH Jeon <mhjeon@modernlab.io>
2017-08-30 17:12:18 +0900

    Init brew-cask list

---
+++ brew/brew-cask.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+brew cask install google-chrome
+brew cask install google-drive
+
+brew cask install dropbox
+
+brew cask install 1password
+
+brew cask install slack
+
+brew cask install atext
+
+brew cask install alfred
+
+brew cask install iterm2
+brew cask install sublime-text
+
+brew cask install spectacle
+
+brew cask install bettertouchtool
+brew cask install macdown
+brew cask install cleanmymac
+brew cask install dash
+brew cask install moom
